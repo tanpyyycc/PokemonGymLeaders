526c69eeea11138a2a5b27042033dece8a349312
tan_e <tan_e@f213.ub.lan>
2026-09-24 09:28:28 +0800

    feat: Created a Trainers Repository

--- src/app/trainer-service/trainer-service.ts
+++ src/app/trainer-service/trainer-service.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Component({
   imports: [],
@@ -6,4 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './trainer-service.css',
   templateUrl: './trainer-service.html',
 })
-export class TrainerService {}
+export class TrainerService {
+  private registry = signal([
+    {
+      name: 'Ash Ketchum',
+      team: ['Pikachu', 'Charizard'],
+      items: ['Light Ball', 'Charizardite Y']
+    },
+    {
+      name: 'Misty',
+      team: ['Starmie', 'Psyduck', 'Gyarados'],
+      items: ['Mystic Water', 'Gyaradosite']
+    },
+    {
+      name: 'Dawn',
+      team: ['Piplup', 'Pachirisu', 'Mamoswine', 'Quilava', 'Togekiss'],
+      items: ['None']
+    },
+    {
+      name: 'Serena',
+      team: ['Delphfox', 'Panchan', 'Sylveon'], 
+      items: ['Focus Sash']
+    },
+    {
+      name: 'Clement',
+      team: ['Luxray', 'Chespin'],
+      items: 'None'
+    }
+  ]); 
+    
+  trainers = this.registry.asReadonly();
+}
